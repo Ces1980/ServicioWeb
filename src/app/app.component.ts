6eebee34f8858341fe15e9afb13ce88e3424ac7d
César Vargas Escorcia <thecesarvargas@gmail.com>
2019-12-05 14:04:34 -0600

    Creación de métodos Listar() y Nuevo()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+/* Importación de router */
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ServicioWeb';
+  /*Se crea una variable privada dentro del constructor llamada router del tipo ROUTER
+  y se hace la importación de Router  */
+  constructor(private router: Router) { }
+
+  Listar() {
+    /* "listar" es la palabra usada en el enrutamiento(app.routing.module) para reedirigir
+    la funcionalidad cuando se use el boton listar (listar usuarios)" */
+    this.router.navigate(["listar"]);
+  }
+  Nuevo() {
+    /* "nuevo" es la palabra usada en el enrutamiento(app.routing.module) para reedirigir
+   la funcionalidad cuando se use el boton Nuevo (Agregar nuevo usuario) " */
+    this.router.navigate(["add"]);
+  }
 }
